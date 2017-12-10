5fff9f2458319b8070dfe06cf7d53cb09a3307c2
Miles Z. Sterrett <miles@mileszs.com>
2017-12-10 17:11:55 -0500

    Setup app for Heroku deployment!

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,9 @@ use Mix.Config
 # which you typically run after static files are built.
 config :discworld_quotes, DiscworldQuotesWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: "discworldquotes.herokuapp.com", port: 80],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -59,6 +60,8 @@ config :logger, level: :info
 #     config :discworld_quotes, DiscworldQuotesWeb.Endpoint, server: true
 #
 
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
+config :discworld_quotes, DiscworldQuotes.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true

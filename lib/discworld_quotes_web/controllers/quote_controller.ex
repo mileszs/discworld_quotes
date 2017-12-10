f801586b3d98af24619be6467a72d1da8343387b
Miles Z. Sterrett <miles@mileszs.com>
2017-12-10 16:10:20 -0500

    Remove unused endpoints

--- lib/discworld_quotes_web/controllers/quote_controller.ex
+++ lib/discworld_quotes_web/controllers/quote_controller.ex
@@ -11,32 +11,8 @@ defmodule DiscworldQuotesWeb.QuoteController do
     render(conn, "index.json", quotes: quotes)
   end
 
-  def create(conn, %{"quote" => quote_params}) do
-    with {:ok, %Quote{} = quote} <- Quotes.create_quote(quote_params) do
-      conn
-      |> put_status(:created)
-      |> put_resp_header("location", quote_path(conn, :show, quote))
-      |> render("show.json", quote: quote)
-    end
-  end
-
   def show(conn, %{"id" => id}) do
     quote = Quotes.get_quote!(id)
     render(conn, "show.json", quote: quote)
-  end
-
-  def update(conn, %{"id" => id, "quote" => quote_params}) do
-    quote = Quotes.get_quote!(id)
-
-    with {:ok, %Quote{} = quote} <- Quotes.update_quote(quote, quote_params) do
-      render(conn, "show.json", quote: quote)
-    end
-  end
-
-  def delete(conn, %{"id" => id}) do
-    quote = Quotes.get_quote!(id)
-    with {:ok, %Quote{}} <- Quotes.delete_quote(quote) do
-      send_resp(conn, :no_content, "")
-    end
   end
 end

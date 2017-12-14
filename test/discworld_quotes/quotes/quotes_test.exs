d88e37531824f6949a3dbb59ef6e1920973d12f7
Miles Z. Sterrett <miles@mileszs.com>
2017-12-13 21:58:52 -0500

    Test Quotes.search_quotes

--- test/discworld_quotes/quotes/quotes_test.exs
+++ test/discworld_quotes/quotes/quotes_test.exs
@@ -7,6 +7,8 @@ defmodule DiscworldQuotes.QuotesTest do
     alias DiscworldQuotes.Quotes.Quote
 
     @valid_attrs %{source: "some source", value: "some value"}
+    @valid_attrs2 %{source: "a source", value: "a value"}
+    @valid_attrs3 %{source: "different", value: "differenter"}
     @update_attrs %{source: "some updated source", value: "some updated value"}
     @invalid_attrs %{source: nil, value: nil}
 
@@ -22,6 +24,13 @@ defmodule DiscworldQuotes.QuotesTest do
     test "list_quotes/0 returns all quotes" do
       quote = quote_fixture()
       assert Quotes.list_quotes() == [quote]
+    end
+
+    test "search_quotes/1 returns matching quotes" do
+      quote_fixture()
+      quote_fixture(@valid_attrs2)
+      quote_fixture(@valid_attrs3)
+      assert [%Quote{source: "some source"}, %Quote{source: "a source"}] = Quotes.search_quotes("source")
     end
 
     test "get_quote!/1 returns the quote with given id" do
